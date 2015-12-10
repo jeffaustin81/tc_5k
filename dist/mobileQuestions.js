3f6cdc7da1a6eeeeb86b08dcd12c08a51a8b27e6
Austin Blanchard <austinpdx@gmail.com>
2015-12-09 23:32:29 -0800

    Add first clickable faq question

---
+++ dist/mobileQuestions.js
@@ -0,0 +1,9 @@
+// function myFunction() {
+//     document.getElementById("mobile-question-1").innerHTML = "YOU CLICKED ME!";
+// }
+
+function show() {
+  var showme = document.getElementById("mobile-answer-1");
+  showme.style.visibility = "visible";
+  showme.style.display = "inline";
+}
