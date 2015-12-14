ac0457377d5f351197b521cbfe40753c66ab1e0e
Austin Blanchard <austinpdx@gmail.com>
2015-12-13 22:52:56 -0800

    Make menu work from registration page by hiding merch sections and showing main sections with jquery and letting page-scroll work normally

--- dist/externalPageMenu.js
+++ dist/externalPageMenu.js
@@ -1,21 +1,35 @@
-function externalMenu() {
+function externalMenuHome() {
   var external = document.getElementById("splash");
 
   if (document.getElementById("splash").style['display'] == 'none') {
-    location.reload();
+    $("#merch").hide();
+    $("#merch-test").hide();
+    $("#splash").fadeIn();
+    $("#race-details").fadeIn();
+    $("#about").fadeIn();
   }
 }
 
+function externalMenuRaceDetails() {
+  var external = document.getElementById("race-details");
 
+  if (document.getElementById("race-details").style['display'] == 'none') {
+    $("#merch").hide();
+    $("#merch-test").hide();
+    $("#splash").fadeIn();
+    $("#race-details").fadeIn();
+    $("#about").fadeIn();
+  }
+}
 
+function externalMenuAbout() {
+  var external = document.getElementById("about");
 
-// if #splash is hidden
-//   location.reload
-//   class="page-scroll" href="#wherever"
-//
-//
-//   $(document).ready(function() {
-//     $("#home-logo").click(function() {
-//       location.reload();
-//     });
-//   });
+  if (document.getElementById("about").style['display'] == 'none') {
+    $("#merch").hide();
+    $("#merch-test").hide();
+    $("#splash").fadeIn();
+    $("#race-details").fadeIn();
+    $("#about").fadeIn();
+  }
+}
