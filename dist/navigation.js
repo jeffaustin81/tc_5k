f6fca2c430f05a42be2e0bed705bf4e7b64e6ba1
Austin Blanchard <austinpdx@gmail.com>
2016-01-05 00:06:44 -0800

    Change navbar icon positioning for mobile

--- dist/navigation.js
+++ dist/navigation.js
@@ -12,6 +12,8 @@ $("#register-now").click(function(){
 $("#race-details-mobile-link").click(function(){
     window.scrollTo(0, 0);
     $("#race-details").fadeIn();
+    $("#map").fadeIn();
+
     $("#splash").hide(); // or .fadeIn(), etc.
     $("#merch").hide();
     $("#merch-test").hide();
