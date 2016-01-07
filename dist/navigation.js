587ecd0e43fe37ed16876218aa11142d5bd99074
Austin Blanchard <austinpdx@gmail.com>
2016-01-07 01:04:21 -0800

    change title to just registration

--- dist/navigation.js
+++ dist/navigation.js
@@ -1,11 +1,12 @@
 $("#register-now").click(function(){
-    $("#merch").fadeIn();
+    $("#merch").hide();
+    $("#merch-mobile").fadeIn();
     $("#merch-test").fadeIn();
     $("#splash").hide(); // or .fadeIn(), etc.
     $("#race-details").hide();
     $("#about").hide();
     $("#race-details-mobile").hide();
-    $("#merch-mobile").hide();
+
     $("#social-mobile").hide();
 
 });
@@ -26,13 +27,14 @@ $("#race-details-mobile-link").click(function(){
 
 $("#merch-mobile-link").click(function(){
     window.scrollTo(0, 0);
-    $("#merch").fadeIn();
+    $("#merch").hide();
+    $("#merch-mobile").fadeIn();
     $("#merch-test").fadeIn();
     $("#splash").hide(); // or .fadeIn(), etc.
     $("#race-details").hide();
     $("#about").hide();
     $("#race-details-mobile").hide();
-    $("#merch-mobile").hide();
+
     $("#social-mobile").hide();
 
 });
