3eef4ef87333c13294e38de473fd11f42f18b539
Austin Blanchard <austinpdx@gmail.com>
2016-01-07 15:34:49 -0800

    Fixed tc title

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-$(window).scroll(function(){
-  $(".tc_title").css("opacity", 1 - $(window).scrollTop() / 50);
-});
+// $(window).scroll(function(){
+//   $(".tc_title").css("opacity", 1 - $(window).scrollTop() / 10);
+// });
