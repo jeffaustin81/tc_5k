ba954387cc2dc43c605b1b53ec77a608567faba9
Austin Blanchard <austinpdx@gmail.com>
2016-01-14 14:53:39 -0800

    begin history page

--- dist/navigation.js
+++ dist/navigation.js
@@ -1,90 +1,34 @@
 $("#register-now").click(function(){
     $("#splash").hide();
-    $("#registration-banner").hide();
-    $("#history-banner").hide();
-    $("#race-details-banner").hide();
+    $("#banners-1").hide();
+    $("#banners-2").hide();
     $(".center-banner").hide();
-    $("#tc15-banner").hide();
-    $("#donate-banner").hide();
-    $("#social-banner").hide();
     $("#registration-page").fadeIn();
 });
 
 $("#registration-banner").click(function(){
     window.scrollTo(0, 0);
     $("#splash").hide();
-    $("#registration-banner").hide();
-    $("#history-banner").hide();
-    $("#race-details-banner").hide();
+    $("#banners-1").hide();
+    $("#banners-2").hide();
     $(".center-banner").hide();
-    $("#tc15-banner").hide();
-    $("#donate-banner").hide();
-    $("#social-banner").hide();
     $("#registration-page").fadeIn();
+});
+
+$("#history-banner").click(function(){
+    window.scrollTo(0, 0);
+    $("#splash").hide();
+    $("#banners-1").hide();
+    $("#banners-2").hide();
+    $(".center-banner").hide();
+    $("#history-page").fadeIn();
 });
 
 $("#race-details-banner").click(function(){
     window.scrollTo(0, 0);
     $("#splash").hide();
-    $("#registration-banner").hide();
-    $("#history-banner").hide();
-    $("#race-details-banner").hide();
+    $("#banners-1").hide();
+    $("#banners-2").hide();
     $(".center-banner").hide();
-    $("#tc15-banner").hide();
-    $("#donate-banner").hide();
-    $("#social-banner").hide();
     $("#race-details-page").fadeIn();
 });
-
-$("#merch-mobile-link").click(function(){
-    window.scrollTo(0, 0);
-    $("#merch").hide();
-    $("#merch-mobile").fadeIn();
-    $("#merch-test").fadeIn();
-    $("#splash").hide(); // or .fadeIn(), etc.
-    $("#race-details").hide();
-    $("#about").hide();
-    $("#race-details-mobile").hide();
-
-    $("#social-mobile").hide();
-
-});
-
-$("#about-link").click(function(){
-    window.scrollTo(0, 0);
-    $("#merch").hide();
-    $("#merch-test").hide();
-    $("#splash").hide(); // or .fadeIn(), etc.
-    $("#race-details").hide();
-    $("#about").fadeIn();
-    $("#race-details-mobile").hide();
-    $("#merch-mobile").hide();
-    $("#social-mobile").hide();
-
-});
-
-
-// $(document).ready(function(){
-//   $("#register-now").click(function() {
-//     $("#splash").hide();
-//   )}
-// });
-
-
-// function navigate() {
-//   var splash = document.getElementById("splash");
-//   var details = document.getElementById("race-details");
-//   var about = document.getElementById("about");
-//   var merch = document.getElementById("merch");
-//
-//     splash.style.visibility = "hidden";
-//     splash.style.display = "none";
-//
-//     details.style.visibility = "hidden";
-//     details.style.display = "none";
-//
-//     about.style.visibility = "hidden";
-//     about.style.display = "none";
-//
-//     merch.style.visibility = "visible";
-//     merch.style.display = "inline";
