a8bee331821a91d34943070fe8d518265c669517
Austin Blanchard <austinpdx@gmail.com>
2016-01-15 02:01:30 -0800

    toggle special class to make hamburger menu darker

--- dist/navColor.js
+++ dist/navColor.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#register-now").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
-        $(".hamburger .hamburger__icon").css("background-color", "#464e54");
+        // $(".hamburger .hamburger__icon").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
     });
@@ -12,8 +12,12 @@ $(document).ready(function(){
     $(".nav-color").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
-        $(".hamburger .hamburger__icon").css("background-color", "#464e54");
+        $(".hamburger .hamburger__icon").toggleClass("special");
         // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
     });
 });
+
+// $('span').hover(function(){
+//     $(this).attr('data-content','bar');
+// });
