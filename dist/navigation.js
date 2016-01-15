435e083c8afa4a5d0682ca50a2d347f7b040c421
Austin Blanchard <austinpdx@gmail.com>
2016-01-14 17:48:51 -0800

    clean up index file and begin tc15 page

--- dist/navigation.js
+++ dist/navigation.js
@@ -32,3 +32,12 @@ $("#race-details-banner").click(function(){
     $(".center-banner").hide();
     $("#race-details-page").fadeIn();
 });
+
+$("#tc15-banner").click(function(){
+    window.scrollTo(0, 0);
+    $("#splash").hide();
+    $("#banners-1").hide();
+    $("#banners-2").hide();
+    $(".center-banner").hide();
+    $("#tc15-page").fadeIn();
+});
