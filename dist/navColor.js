1f7a8827ea22df960526540c9da6bc960a66f73e
Austin Blanchard <austinpdx@gmail.com>
2016-01-15 02:07:15 -0800

    change toggle class to add class for hamburger menu

--- dist/navColor.js
+++ dist/navColor.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#register-now").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
+        $(".hamburger .hamburger__icon").toggleClass("special");
         // $(".hamburger .hamburger__icon").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
@@ -12,12 +13,8 @@ $(document).ready(function(){
     $(".nav-color").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
-        $(".hamburger .hamburger__icon").toggleClass("special");
+        $(".hamburger .hamburger__icon").addClass("special");
         // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
     });
 });
-
-// $('span').hover(function(){
-//     $(this).attr('data-content','bar');
-// });
