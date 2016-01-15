aa67e9dc528fb6b3d3fb21abccd0bb09a4380a30
Austin Blanchard <austinpdx@gmail.com>
2016-01-15 02:08:53 -0800

    same but for reg now

--- dist/navColor.js
+++ dist/navColor.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#register-now").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
-        $(".hamburger .hamburger__icon").toggleClass("special");
+        $(".hamburger .hamburger__icon").addClass("special");
         // $(".hamburger .hamburger__icon").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
         // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
