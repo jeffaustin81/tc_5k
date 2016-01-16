6bbff5038c07108a554d9fb150ab113519810306
Austin Blanchard <austinpdx@gmail.com>
2016-01-15 17:39:52 -0800

    disable scrolling when in hamburger menu on web

--- dist/navColor.js
+++ dist/navColor.js
@@ -2,10 +2,7 @@ $(document).ready(function(){
     $("#register-now").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
-        $(".hamburger .hamburger__icon").addClass("special");
-        // $(".hamburger .hamburger__icon").css("background-color", "#464e54");
-        // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
-        // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
+        $(".hamburger .hamburger__icon").addClass("darken");
     });
 });
 
@@ -13,8 +10,7 @@ $(document).ready(function(){
     $(".nav-color").click(function(){
         $(".tc-title").css("color", "#464e54");
         $(".tc-nav-logo").css("fill", "#464e54");
-        $(".hamburger .hamburger__icon").addClass("special");
-        // $(".hamburger .hamburger__icon:before").css("background-color", "#464e54");
-        // $(".hamburger .hamburger__icon:after").css("background-color", "#464e54");
+        $(".hamburger .hamburger__icon").addClass("darken");
+        $("body").removeClass("stop-scrolling");
     });
 });
