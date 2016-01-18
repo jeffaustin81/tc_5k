48100f007fc2f3c141a3b93373cdfaab88903032
Austin Blanchard <austinpdx@gmail.com>
2016-01-18 15:33:22 -0800

    remove tc-title shadow from dark versions

--- dist/navColor.js
+++ dist/navColor.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#register-now").click(function(){
         $(".tc-title").css("color", "#464e54");
+        $(".tc-title").css("text-shadow", "0 0 0 black");
         $(".tc-nav-logo").css("fill", "#464e54");
         $(".hamburger .hamburger__icon").addClass("darken");
     });
@@ -9,6 +10,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".nav-color").click(function(){
         $(".tc-title").css("color", "#464e54");
+        $(".tc-title").css("text-shadow", "0 0 0 black");
         $(".tc-nav-logo").css("fill", "#464e54");
         $(".hamburger .hamburger__icon").addClass("darken");
         $("body").removeClass("stop-scrolling");
