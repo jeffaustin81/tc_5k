e363390b3b005fdcf53e1f061c4845dc510bca09
Austin Blanchard <austinpdx@gmail.com>
2016-01-26 19:49:02 -0800

    change merch layout media query

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,13 +5,13 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
